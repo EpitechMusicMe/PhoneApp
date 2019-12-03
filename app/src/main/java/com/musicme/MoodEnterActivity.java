50c1456b3779c21ef6c547e18cecb1ef8594b2ad
teabolt <tomasbaltrunas0@gmail.com>
2019-12-03 13:43:47 +0100

    Clean up repo

---
+++ app/src/main/java/com/musicme/MoodEnterActivity.java
@@ -0,0 +1,6 @@
+package com.musicme;
+
+public class MoodEnterActivity {
+
+
+}
---
+++ app/src/main/java/com/musicme/SpotifyAuthActivity.java
@@ -0,0 +1,14 @@
+package com.musicme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpotifyAuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spotify_auth);
+    }
+}

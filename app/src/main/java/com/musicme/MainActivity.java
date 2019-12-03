85fb82637791456f6e7a117597e1d3461e8ecb3f
teabolt <tomasbaltrunas0@gmail.com>
2019-12-03 15:12:41 +0100

    Add Spotify SDK dependencies

--- app/src/main/java/com/musicme/MainActivity.java
+++ app/src/main/java/com/musicme/MainActivity.java
@@ -12,6 +12,14 @@ import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
 
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+
 
 public class MainActivity extends AppCompatActivity {
 

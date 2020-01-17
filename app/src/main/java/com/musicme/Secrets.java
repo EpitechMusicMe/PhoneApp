dac658a61bbf2260d53e6aec237a5c46ef1dbe46
teabolt <tomasbaltrunas0@gmail.com>
2020-01-17 01:57:46 +0100

    Move Spotify API secrets to constants file

---
+++ app/src/main/java/com/musicme/Secrets.java
@@ -0,0 +1,9 @@
+package com.musicme;
+
+public class Secrets {
+
+    public static final String CLIENT_ID = "5f2f2cd771174cfcb2e3a8433c47935c";
+    public static final String REDIRECT_URI = "musicme://auth/callback/";
+    // TODO: these secrets should be retrieved from the server instead of being hard-coded
+
+}
